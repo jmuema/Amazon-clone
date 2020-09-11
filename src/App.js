2364a4f304ff64741dfcced4e184de8e2cbe5f5b
muema254 <jojotheone7@gmail.com>
2020-09-11 08:57:47 +0300

    just created the home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
-      <h1> Amazon Clone</h1>
+      {/* <h1> Amazon Clone</h1> */}
 
       {/* Header */}
       <Header/>
+      <Home/>
        {/* Home */}
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1> Home componentDidMount() {
+                
+            }
+            </h1>
+        </div>
+    )
+}
+
+export default Home
