ec1ddff82ea85b4bcb9f9dbc422af16b12308498
muema254 <jojotheone7@gmail.com>
2020-09-11 10:17:46 +0300

    added the product component to the home component

--- src/Home.js
+++ src/Home.js
@@ -19,16 +19,21 @@ function Home() {
                     {/* Product */}
                     <Product />
                     {/* Product */}
+                    <Product />
 
                 </div>
                 <div className="home__row" >
                     {/* Product */}
+                    <Product />
                     {/* Product */}
+                    <Product />
                     {/* Product */}
+                    <Product />
 
                 </div>
                 <div className="home__row" >
                     {/* Product */}
+                    <Product />
 
                 </div>
             </div>
--- src/Product.js
+++ src/Product.js
@@ -3,24 +3,25 @@ import "./Product.css";
 
 function Product() {
     return (
-        <div className="product" >
-            <div className="product__info" >
-                <p>  The lean startup</p>
-                <p className="product__price" >
-                    <small> $</small>
-                    <strong> 19.99</strong>
-                </p>
-
-                <div className="product__rating" >
-                    {/* <p>⭐️</p> */}
-                </div>
-                <img src=" https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt=""/>
-                <button> Add to Basket</button>
-
+        <div className="product">
+          <div className="product__info">
+            <p>The lean startup</p>
+            <p className="product__price">
+              <small>$</small>
+              <strong>19.99</strong>
+            </p>
+            <div className="product__rating">
+              
+                  <p>🌟</p>
             </div>
 
+          </div>
+    
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+    
+          <button >Add to Basket</button>
         </div>
-    )
-}
+      );
+    }
 
 export default Product
